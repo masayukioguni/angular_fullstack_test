2e7c6ef02f63a08aa901bd09de28d0ef23368467
masayuki_oguni <masayuki_oguni@dwango.co.jp>
2014-01-31 15:53:42 +0900

    routeParams

--- app/scripts/controllers/game.js
+++ app/scripts/controllers/game.js
@@ -1,13 +1,15 @@
 'use strict';
 
 angular.module('angularFullstackTestApp')
-  .controller('GameCtrl', function ($scope, $http) {
+  .controller('GameCtrl', function ($scope, $http,$routeParams) {
     $http.get('/api/games').success(function(res) {
+      console.log($routeParams);
       $scope.games = res;
     });
 
     $scope.createGame = function() {
-      console.log($scope.game);
+
+      
       if ($scope.game && $scope.game.name) {
         $http({
           method: 'POST',
@@ -15,9 +17,11 @@ angular.module('angularFullstackTestApp')
           data: $scope.game
         }).success(function() {
           $scope.games.push({
-            name: $scope.game.name
+            name: $scope.game.name,
+            url: $scope.game.url
           });
           $scope.game.name = '';
+          $scope.game.url = '';
         });
       }
     };
